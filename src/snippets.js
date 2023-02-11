d2def65703a194678e9f680100e982404da039ac
Jorge <jorge.elvir@gmail.com>
2023-02-11 09:30:16 -0600

    first commit

---
+++ src/snippets.js
@@ -0,0 +1,15 @@
+const snippets = {
+  snippet : {
+    "name": "",
+    "cats": [],
+    "code": "",
+    "language": `<pre><code data-language="javascript">
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString);
+    const applicationParam = urlParams.get('parameter-name') //parameter name
+    if(applicationParam !== null){
+        // do something
+    }
+              </code></pre>`
+  }
+}
