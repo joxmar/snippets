9c867d872a6da9a7702e1530084245e15d1204c3
Jorge Elvir <jorge.elvir@gmail.com>
2024-03-15 16:43:54 -0500

    fixed formatting on ACF Link Field snippet

--- js/snippets/wp-acf-links.js
+++ js/snippets/wp-acf-links.js
@@ -3,14 +3,14 @@ name : "Advanced Custom Fields ACF - Link field sample",
 cats : ['wordpress', 'ACF'],
 language : "php",
 snippet : `
-<?php 
-  $link = get_sub_field('job_link');
+&lt;?php 
+  $link = get_sub_field('field_name');
   if( $link ): 
     $link_url = $link['url'];
     $link_title = $link['title'];
     $link_target = $link['target'] ? $link['target'] : '_self';
-    ?>
-    <a href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>            
-<?php endif; ?>
+    ?&gt;
+    &lt;a href="&lt;?php echo esc_url( $link_url ); ?&gt;" target="&lt;?php echo esc_attr( $link_target ); ?&gt;"&gt;&lt;?php echo esc_html( $link_title ); ?&gt;&lt;/a&gt;            
+&lt;?php endif; ?&gt;
 `
 }
