78443c114d0167944d039da59a9912885dac7fee
Jorge Elvir <jorge.elvir@gmail.com>
2024-04-05 16:59:44 -0500

    added more examples to js while loop

--- js/snippets/js-while-loop.js
+++ js/snippets/js-while-loop.js
@@ -15,5 +15,25 @@ while (dice !== 6) {
     console.log('You won! Rolled a 6!');
   }
 }
+
+// while loop with counter
+let i = 0;
+while (i < 5) {
+  console.log('Hello world ' + i);
+  i++;
+
+  // if it reaches 5, then do something right before exiting the loop
+  if (i === 5) {
+    console.log("You've reached 5, buh-bye!");
+  }
+}
+/* returns:
+Hello world 0
+Hello world 1
+Hello world 2
+Hello world 3
+Hello world 4
+You've reached 5, buh-bye!
+*/
 `
 }
