8fd221bfa3fc0bdf8ac61ddc76f18e0cf6df6d9a
Jorge Elvir <jorge.elvir@gmail.com>
2024-05-02 11:11:37 -0500

    added image uploader snippet

--- js/app.js
+++ js/app.js
@@ -1,39 +1,36 @@
-import { gutenAddCSS } from "./snippets/wp-add-css-to-gutenberg.js";
-import { autoLogin } from "./snippets/wp-auto-login.js";
-import { jsGetURL } from "./snippets/js-get-url-parameters.js";
-import { gutBlocksToDisplay } from "./snippets/wp-gutenberg-select-blocks-to-display.js";
-import { mailchimpSuccess } from "./snippets/mailchimp-success-callback.js";
-import { jsCreateElement } from "./snippets/js-create-element.js";
-import { jsaddRemoveHasClass } from "./snippets/js-add-remove-class.js";
-import { jsGetPrevNextSibling } from "./snippets/js-get-previous-next-sibling.js";
-import { jsCopyToClipboard } from "./snippets/js-copy-text-to-clipboard.js";
-import { wpLoadArchivePlugin } from "./snippets/wp-plugin-add-archive-templatephp.js";
-import { npmDropboxIgnoreNodeModules } from "./snippets/npm-dropbox-ignore-node-modules.js";
-import { gutAcfColorRadios } from "./snippets/wp-acf-radio-color-picker.js";
-import { registerAcfBlockTemplate } from "./snippets/wp-registrer-ACF-block-template.js";
-import { wpGetAllCpt } from "./snippets/wp-get-all-registered-cpt.js";
-import { wpChangeLoginLogo } from "./snippets/wp-change-Login-logo-and-link-URL.js";
-import { wpBlogSearchArchiveShortcodes } from "./snippets/wp-create-blog-search-archive-loop-shortcodes.js";
-import { wpCustomizeComments } from "./snippets/wp-customize-comment-fields.js";
-import { wpListPostCptCategories } from "./snippets/wp-list-post-cpt-categories.js";
-import { gutenbergResizableBar } from "./snippets/wp-gutenberg-resizable-sidebar.js";
-import { overrideAcfWysiwygColors } from "./snippets/wp-override-acf-wysiwyg-colors.js";
-import { wpAcfLink } from "./snippets/wp-acf-links.js";
-import { updateArrays } from "./snippets/js-updating-arrays.js";
-import { jsforloop } from "./snippets/js-for-loop.js";
-import { jsWhileLoop } from "./snippets/js-while-loop.js";
-import { diviFormSenderEmail } from "./snippets/wp-divi-change-form-sender.js";
-
-
-
-
+import { gutenAddCSS } from './snippets/wp-add-css-to-gutenberg.js';
+import { autoLogin } from './snippets/wp-auto-login.js';
+import { jsGetURL } from './snippets/js-get-url-parameters.js';
+import { gutBlocksToDisplay } from './snippets/wp-gutenberg-select-blocks-to-display.js';
+import { mailchimpSuccess } from './snippets/mailchimp-success-callback.js';
+import { jsCreateElement } from './snippets/js-create-element.js';
+import { jsaddRemoveHasClass } from './snippets/js-add-remove-class.js';
+import { jsGetPrevNextSibling } from './snippets/js-get-previous-next-sibling.js';
+import { jsCopyToClipboard } from './snippets/js-copy-text-to-clipboard.js';
+import { wpLoadArchivePlugin } from './snippets/wp-plugin-add-archive-templatephp.js';
+import { npmDropboxIgnoreNodeModules } from './snippets/npm-dropbox-ignore-node-modules.js';
+import { gutAcfColorRadios } from './snippets/wp-acf-radio-color-picker.js';
+import { registerAcfBlockTemplate } from './snippets/wp-registrer-ACF-block-template.js';
+import { wpGetAllCpt } from './snippets/wp-get-all-registered-cpt.js';
+import { wpChangeLoginLogo } from './snippets/wp-change-Login-logo-and-link-URL.js';
+import { wpBlogSearchArchiveShortcodes } from './snippets/wp-create-blog-search-archive-loop-shortcodes.js';
+import { wpCustomizeComments } from './snippets/wp-customize-comment-fields.js';
+import { wpListPostCptCategories } from './snippets/wp-list-post-cpt-categories.js';
+import { gutenbergResizableBar } from './snippets/wp-gutenberg-resizable-sidebar.js';
+import { overrideAcfWysiwygColors } from './snippets/wp-override-acf-wysiwyg-colors.js';
+import { wpAcfLink } from './snippets/wp-acf-links.js';
+import { updateArrays } from './snippets/js-updating-arrays.js';
+import { jsforloop } from './snippets/js-for-loop.js';
+import { jsWhileLoop } from './snippets/js-while-loop.js';
+import { diviFormSenderEmail } from './snippets/wp-divi-change-form-sender.js';
+import { phpAjaxImgUploader } from './snippets/php-js-image-uploader.js';
 
 // add snippet into the array
 let allSnippets = [
   gutenAddCSS,
   jsGetURL,
   autoLogin,
-  gutBlocksToDisplay, 
+  gutBlocksToDisplay,
   mailchimpSuccess,
   jsCreateElement,
   jsaddRemoveHasClass,
@@ -54,7 +51,8 @@ let allSnippets = [
   updateArrays,
   jsforloop,
   jsWhileLoop,
-  diviFormSenderEmail
+  diviFormSenderEmail,
+  phpAjaxImgUploader,
 ];
 
 // we will use this array to save the snippet names and use it on autocomplete
@@ -66,61 +64,56 @@ let snippetCategories = [];
 // start tab counter
 let tabsCounter = 0;
 
-
-
 // build snippets into the DOM
 let snippetsContainer = document.getElementById('snippets-container');
 
 for (let i = 0; i < allSnippets.length; i++) {
   // get the snippet details from object
   const snippetName = allSnippets[i].name;
-  const snippetCats = allSnippets[i].cats.map(word => word.toLowerCase());
+  const snippetCats = allSnippets[i].cats.map((word) => word.toLowerCase());
   const snippetLanguage = allSnippets[i].language;
   const theSnippet = allSnippets[i].snippet;
 
-  
-  
   // loop and add categories to main categories array
   for (let i = 0; i < snippetCats.length; i++) {
-    if(!snippetCategories.includes(snippetCats[i])){
+    if (!snippetCategories.includes(snippetCats[i])) {
       snippetCategories.push(snippetCats[i]);
-    };
+    }
   }
-  
-
-
 
   // push to snippet names list for auto complete
-  snippetNames.push(snippetName)
-
-
+  snippetNames.push(snippetName);
 
   // create snippet container
   const snippetContainer = document.createElement('article');
-  snippetContainer.classList.add('snippet-container', 'bg-white', 'rounded-xl', 'drop-shadow-2xl', 'mt-10');
+  snippetContainer.classList.add(
+    'snippet-container',
+    'bg-white',
+    'rounded-xl',
+    'drop-shadow-2xl',
+    'mt-10'
+  );
   snippetContainer.setAttribute('data-categories', snippetCats);
-  snippetContainer.setAttribute('id', snippetName.replace(/ /g,'-'));
+  snippetContainer.setAttribute('id', snippetName.replace(/ /g, '-'));
 
-
-  
   // snippet details
   const snippetdetails = document.createElement('div');
   snippetdetails.classList.add('p-10');
 
   const snippetdetailsCats = document.createElement('div');
   snippetdetailsCats.classList.add('categories');
-  const snippetdetailsCatsText = document.createTextNode(snippetCats.join(', '));
+  const snippetdetailsCatsText = document.createTextNode(
+    snippetCats.join(', ')
+  );
   snippetdetailsCats.appendChild(snippetdetailsCatsText);
 
   const snippetdetailsName = document.createElement('h2');
   snippetdetailsName.classList.add('snippet-title');
   const snippetdetailsNameText = document.createTextNode(snippetName);
   snippetdetailsName.appendChild(snippetdetailsNameText);
-  
+
   snippetdetails.appendChild(snippetdetailsCats);
   snippetdetails.appendChild(snippetdetailsName);
-
-
 
   // snippet code
   const snippetCodeContainer = document.createElement('div');
@@ -134,22 +127,21 @@ for (let i = 0; i < allSnippets.length; i++) {
   copySnippetBtn.textContent = 'Copy';
 
   // if is single snippet or multiple
-  if(typeof theSnippet === 'string'){
+  if (typeof theSnippet === 'string') {
     snippetCodeTag.innerHTML = theSnippet;
     snippetPreTag.appendChild(snippetCodeTag);
     snippetPreTag.appendChild(copySnippetBtn);
 
     snippetCodeContainer.appendChild(snippetPreTag);
   } else {
-    const [tabBtnsContainer, tabsContainer] = buildTabs(theSnippet, snippetLanguage);
+    const [tabBtnsContainer, tabsContainer] = buildTabs(
+      theSnippet,
+      snippetLanguage
+    );
     // snippetCodeContainer.appendChild(buildTabs(theSnippet));
     snippetCodeContainer.appendChild(tabBtnsContainer);
     snippetCodeContainer.appendChild(tabsContainer);
   }
-
-
-  
-
 
   // now append details to the snippet container
   snippetContainer.appendChild(snippetdetails);
@@ -158,9 +150,6 @@ for (let i = 0; i < allSnippets.length; i++) {
   // and finally to the main container on the DOM
   snippetsContainer.appendChild(snippetContainer);
 }
-
-
-
 
 // buil setup categories sidebar
 let menuList = document.getElementById('cat-links');
@@ -173,7 +162,11 @@ for (let i = 0; i < snippetCategories.length; i++) {
   // create button
   const menuCategoryItemBtn = document.createElement('button');
   menuCategoryItemBtn.setAttribute('data-category-type', snippetCategories[i]);
-  menuCategoryItemBtn.classList.add('text-bcolor-3','hover:text-bcolor-2', 'select-cat');
+  menuCategoryItemBtn.classList.add(
+    'text-bcolor-3',
+    'hover:text-bcolor-2',
+    'select-cat'
+  );
 
   // create and add category text to button
   const menuCategoryItemText = document.createTextNode(snippetCategories[i]);
@@ -184,7 +177,6 @@ for (let i = 0; i < snippetCategories.length; i++) {
 
   // add li to ul
   menuList.appendChild(menuCategoryItem);
-  
 }
 
 // add event to cat links
@@ -192,78 +184,77 @@ const categoryBtn = document.querySelectorAll('.select-cat');
 const snippetContainers = document.querySelectorAll('.snippet-container');
 
 // go through each button
-categoryBtn.forEach(el => {
+categoryBtn.forEach((el) => {
   // add click event listener
-  el.addEventListener('click', function(){
+  el.addEventListener('click', function () {
     // get this buttons category data-attr
     let elCategoryType = el.getAttribute('data-category-type');
     // loop through all the snippet containers
-    snippetContainers.forEach(element => {
+    snippetContainers.forEach((element) => {
       // get current nippet container categories in the data attr
-      let containerCats = element.getAttribute('data-categories')
-      // remove hide class to clear all snippet containers 
+      let containerCats = element.getAttribute('data-categories');
+      // remove hide class to clear all snippet containers
       element.classList.remove('hide-snippet');
       // search the button's category inside the snippet container categories
-      if(!containerCats.includes(elCategoryType)){ //if value is found true
+      if (!containerCats.includes(elCategoryType)) {
+        //if value is found true
         element.classList.add('hide-snippet');
       }
     });
-  })
+  });
 });
 
 // clear all button
 const showAllSnippets = document.getElementById('show-all');
-showAllSnippets.addEventListener('click', function(){
-  snippetContainers.forEach(element => {
+showAllSnippets.addEventListener('click', function () {
+  snippetContainers.forEach((element) => {
     element.classList.remove('hide-snippet');
   });
 });
-
 
 // initiate auto complete
-const autoCompleteJS = new autoComplete({ 
-  selector: "#autocomplete",
-  placeHolder: "Search for code...",
+const autoCompleteJS = new autoComplete({
+  selector: '#autocomplete',
+  placeHolder: 'Search for code...',
   data: {
-    src: snippetNames
+    src: snippetNames,
   },
   resultItem: {
-    highlight: "autoComplete_highlight",
-    selected: "autoComplete_selected"
-  }
- });
-
- document.querySelector("#autocomplete").addEventListener("selection", function (event) {
-
-  // clear any hidden snippets
-  snippetContainers.forEach(element => {
-    element.classList.remove('hide-snippet');
-  });
-
-  let codeName = event.detail.selection.value;
-  let codeAnchor = document.getElementById(codeName.replace(/ /g, '-'));
-  let headerOffset = 90;
-  let elementPosition = codeAnchor.getBoundingClientRect().top;
-  let offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-    window.scrollTo({
-         top: offsetPosition,
-        //  behavior: "smooth"
-    });
-
+    highlight: 'autoComplete_highlight',
+    selected: 'autoComplete_selected',
+  },
 });
 
+document
+  .querySelector('#autocomplete')
+  .addEventListener('selection', function (event) {
+    // clear any hidden snippets
+    snippetContainers.forEach((element) => {
+      element.classList.remove('hide-snippet');
+    });
 
-function buildTabs(snippet, language){
+    let codeName = event.detail.selection.value;
+    let codeAnchor = document.getElementById(codeName.replace(/ /g, '-'));
+    let headerOffset = 90;
+    let elementPosition = codeAnchor.getBoundingClientRect().top;
+    let offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    window.scrollTo({
+      top: offsetPosition,
+      //  behavior: "smooth"
+    });
+  });
+
+function buildTabs(snippet, language) {
   // loop and get all tabNames
   const tabBtnsContainer = document.createElement('ul');
   const tabsContainer = document.createElement('div');
-  tabBtnsContainer.classList.add('tab-btns', 'flex', 'px-10', 'gap-8');  
+  tabBtnsContainer.classList.add('tab-btns', 'flex', 'px-10', 'gap-8');
   for (let i = 0; i < snippet.length; i++) {
     // create tabs elemens with ul li and button
     const tabsLiContainer = document.createElement('li');
     const tabBtn = document.createElement('button');
     tabBtn.classList.add('py-4');
-    if(i == 0){
+    if (i == 0) {
       tabBtn.classList.add('font-bold');
     }
     tabBtn.setAttribute('data-tab-content', 'tab-' + tabsCounter);
@@ -273,13 +264,13 @@ function buildTabs(snippet, language){
     const copySnippetBtn = document.createElement('button');
     copySnippetBtn.classList.add('copy-snippet', 'rounded-md', 'py-2', 'px-4');
     copySnippetBtn.textContent = 'Copy';
-    
+
     // create tab content
     const snippetPreTag = document.createElement('pre');
-    
+
     snippetPreTag.classList.add('tab-container');
     //add class hidden to all items except the first one
-    if(i > 0){
+    if (i > 0) {
       snippetPreTag.classList.add('hidden');
     }
     const snippetCodeTag = document.createElement('code');
@@ -299,17 +290,15 @@ function initTabs() {
   const tabButtons = document.querySelectorAll('.tab-btns button');
   const tabContent = document.querySelectorAll('.tab-container');
 
-
-  tabButtons.forEach(button => {
+  tabButtons.forEach((button) => {
     button.addEventListener('click', () => {
-      tabButtons.forEach(btn => {
+      tabButtons.forEach((btn) => {
         btn.classList.remove('font-bold');
       });
       button.classList.add('font-bold');
 
-      
       const target = button.getAttribute('data-tab-content');
-      tabContent.forEach(tab => {
+      tabContent.forEach((tab) => {
         tab.classList.add('hidden');
         if (tab.getAttribute('id') === target) {
           tab.classList.remove('hidden');
@@ -321,10 +310,10 @@ function initTabs() {
 
 initTabs();
 
-function copySnippetFunction(){
+function copySnippetFunction() {
   const copySnippetBtns = document.querySelectorAll('.copy-snippet');
-  copySnippetBtns.forEach(btn => {
-    btn.addEventListener('click', function(){          
+  copySnippetBtns.forEach((btn) => {
+    btn.addEventListener('click', function () {
       let codeContainer = this.previousElementSibling;
       var r = document.createRange();
       r.selectNode(codeContainer);
@@ -338,8 +327,7 @@ function copySnippetFunction(){
         setTimeout(() => {
           btn.textContent = 'Copy';
         }, 1000);
-      }
-      , 100);
+      }, 100);
     });
   });
 }
---
+++ js/snippets/php-js-image-uploader.js
@@ -0,0 +1,85 @@
+export const phpAjaxImgUploader = {
+  name: 'Upload images to server using AJAX and PHP',
+  cats: ['javascript', 'php', 'ajax'],
+  language: 'html',
+  snippet: [
+    {
+      tabName: 'index.php',
+      content: `
+      <!-- make sure to create the uploads folder first! -->
+      <form id="upload_form" enctype="multipart/form-data">
+    <input type="file" name="file" id="file">
+    <input type="button" value="Upload File" onclick="uploadFile()">
+  </form>
+  <div id="upload_status"></div>
+  <img id="uploaded_image" src="" alt="">
+  <script>
+    // get curreent site url
+    var site_url = window.location.href;
+    function uploadFile(){
+      var file = document.getElementById("file").files[0];
+      var formdata = new FormData();
+      formdata.append("file", file);
+      var ajax = new XMLHttpRequest();
+      ajax.upload.addEventListener("progress", progressHandler, false);
+      ajax.addEventListener("load", completeHandler, false);
+      ajax.addEventListener("error", errorHandler, false);
+      ajax.addEventListener("abort", abortHandler, false);
+      ajax.open("POST", "uploader.php");
+      ajax.send(formdata);
+    }
+    function progressHandler(event){
+      document.getElementById("upload_status").innerHTML = "Upload Progress: " + event.loaded + " / " + event.total;
+    }
+    function completeHandler(event){
+      console.log(event.target.responseText);
+      var response = JSON.parse(event.target.responseText);      
+      document.getElementById("upload_status").innerHTML = "Upload Complete";
+      document.getElementById("uploaded_image").src = site_url + response.fileLocation;
+    }
+    function errorHandler(event){
+      document.getElementById("upload_status").innerHTML = "Upload Failed";
+    }
+    function abortHandler(event){
+      document.getElementById("upload_status").innerHTML = "Upload Aborted";
+    }
+  </script>`,
+    },
+    {
+      tabName: 'uploader.php',
+      content: `
+      if(isset($_FILES['file'])){
+        $file = $_FILES['file'];
+        $fileName = $_FILES['file']['name'];
+        $fileTmpName = $_FILES['file']['tmp_name'];
+        $fileSize = $_FILES['file']['size'];
+        $fileError = $_FILES['file']['error'];
+        $fileType = $_FILES['file']['type'];
+        
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+        
+        $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+        
+        if(in_array($fileActualExt, $allowed)){
+          if($fileError === 0){
+            if($fileSize < 1000000){
+              $fileNameNew = uniqid('', true).".".$fileActualExt;
+              $fileDestination = 'uploads/'.$fileNameNew;
+              move_uploaded_file($fileTmpName, $fileDestination);
+              // echo out a JSON response with the file location
+              echo json_encode(['fileLocation' => $fileDestination]);
+            }else{
+              echo "Your file is too big!";
+            }
+          }else{
+            echo "There was an error uploading your file!";
+          }
+        }else{
+          echo "You cannot upload files of this type!";
+        }
+      }
+      `,
+    },
+  ],
+};
