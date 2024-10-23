0e4768fd9f46f26c2c506a6c51ce28631a7f9c85
Jorge Elvir <jorge.elvir@gmail.com>
2024-10-23 10:11:55 -0500

    updated GET URL snippet to include a PHP snippet as well

--- js/snippets/js-get-url-parameters.js
+++ js/snippets/js-get-url-parameters.js
@@ -1,13 +1,28 @@
 export const jsGetURL = {
-name : "Get URL Parameters",
-cats : ['javascript', 'URL'],
-language : "Javascript",
-snippet : `
+  name: 'Get URL Parameters',
+  cats: ['javascript', 'php'],
+  language: 'Javascript',
+
+  snippet: [
+    {
+      language: 'javascript',
+      tabName: 'Javascript',
+      content: `
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString);
 const applicationParam = urlParams.get('parameter-name') //parameter name
 if(applicationParam !== null){
     // do something
-}
-`
-}
+}`,
+    },
+    {
+      language: 'php',
+      tabName: 'PHP',
+      content: `
+if (isset($_GET['param-name'])) {
+  $param_value = $_GET['param-name'];
+  // do something
+}`,
+    },
+  ],
+};
