ab240678af2927aa1349c9f6ba1fba86fff1ccb8
Jorge Elvir <jorge.elvir@gmail.com>
2026-02-17 16:55:17 -0600

    enhanced ACF image field snippet with recommended usage and improved alt text handling

--- js/snippets/wp-acf-image-field.js
+++ js/snippets/wp-acf-image-field.js
@@ -3,9 +3,20 @@ export const wpAcfImg = {
   cats: ['wordpress', 'acf', 'php'],
   language: 'php',
   snippet: `
+// RECOMMENDED: ACF image field set to return an ID 
+// using the WordPress function wp_get_attachment_image() 
+// This renders responsive images (srcset) and includes the correct alt text and classes.
+&lt;?php if (get_field('image')): 
+  $image_id = get_field('image');	
+  $image_size = 'large'; // (thumbnail, medium, large, full or custom size)							
+  echo wp_get_attachment_image( $image_id, $image_size, '', array( 'class' => 'class_names' ) );
+endif; ?&gt;
+
+// A more manual way
+// ACF image when set to return an array
 &lt;?php if (get_field('image_field')):
   $image = get_field('image_field'); ?&gt;
-<img src="&lt;?php echo $image['sizes']['medium']; ?&gt;" alt="&lt;?php echo $image['alt']; ?&gt;">
+<img src="&lt;?php echo $image['sizes']['medium']; ?&gt;" alt="&lt;?php echo $image['alt']; ?&gt;" class="class_names">
 &lt;?php endif; ?&gt;
 `,
 };
