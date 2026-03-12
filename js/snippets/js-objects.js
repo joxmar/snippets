135477d9be168c0765d8f0e4ca3b57a119778b90
Jorge Elvir <jorge.elvir@gmail.com>
2026-03-12 14:10:00 -0500

    enhance(jsObjectsTips): improve object manipulation examples and add explanations

--- js/snippets/js-objects.js
+++ js/snippets/js-objects.js
@@ -3,17 +3,34 @@ export const jsObjectsTips = {
   cats: ['javascript'],
   language: 'javascript',
   snippet: `
-// Get the values only from an object
+// Object
 const person = {
   name: 'Jorge',
   age: 43,
   city: 'Tegucigalpa'
 };
 
-// Object.values() method returns an array of of the values of an object.
+// CORRECT WAY to loop through an object with Object.entries()
+// This method returns an array of the key-value pairs of an object.
+for (const [key, value] of Object.entries(person)) {
+  console.log(\`\${key}: \${value}\`);
+}
+// Output:
+// name: Jorge
+// age: 43
+// city: Tegucigalpa
+
+// Get the values only from an object with Object.values(). 
+// This method returns an array of of the values of an object.
 const values = Object.values(person);
 console.log(values);
 // Output: ["Jorge", 43, "Tegucigalpa"]
+
+// Get the keys only from an object with Object.keys().
+// This method returns an array of the keys of an object.
+const keys = Object.keys(person);
+console.log(keys);
+// Output: ["name", "age", "city"]
 
 `,
 };
